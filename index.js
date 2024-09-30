f98b59a4fbba5aa39613c4e03cd00d388b41f7fb
sktanui <shadrackkemei2019@gmail.com>
2024-09-30 12:52:31 +0300

    complete array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === names.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, names) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().startsWith(names.toLowerCase())
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase()
+
+)
+}
